fb743160d2c568dcbdfcae2ad381b16e67e2657c
minsangKwak <kmsdevwork@gmail.com>
2025-12-12 16:41:38 +0900

    chore: set up github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+const repoName = process.env.GITHUB_REPOSITORY?.split("/").pop();
+
 export default defineConfig({
+  base: repoName ? `/${repoName}/` : "/",
   plugins: [react()],
 });
