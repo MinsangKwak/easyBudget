d9ec78d250d7720d3b5a19f42548ed2981fa3923
minsangKwak <kmsdevwork@gmail.com>
2025-12-18 13:38:57 +0900

    Refactor login selection flow

--- src/components/Screen/Intro/index.jsx
+++ src/components/Screen/Intro/index.jsx
@@ -3,10 +3,12 @@ import BaseButton from "../../Form/BaseButton";
 import Screen from "../../Layout/Screen";
 import Title from "../../Content/Title";
 import Inner from "../../Content/Inner";
+
+import Subtitle from "../../Content/SubTitle";
 // import LottieIcon from "../../Common/LottieIcon";
 // import { LOTTIE_SOURCES } from "../../../constants/lottieSources";
 
-const ScreenIntro = ({ onClickGoJoin }) => {
+const ScreenIntro = ({ onClickLoginEmail, onClickLoginCertificate, onClickGoJoin }) => {
   return (
     <Screen className="screen_main">
       {/* <div className="screen_visual">
@@ -15,17 +17,37 @@ const ScreenIntro = ({ onClickGoJoin }) => {
           ariaLabel="인증 안내 애니메이션"
         />
       </div> */}
-      <Title>안녕하세요 <br/ >:)</Title>
+      <Title>
+        로그인 방식을
+        <br />
+        선택해주세요
+      </Title>
+      <Subtitle>시작하기 전에 로그인 방식을 먼저 선택해주세요.</Subtitle>
       <Inner>
-        <p className="intro_text">
-          원하시는 방식으로 로그인/회원가입을 진행해주세요.
-        </p>
+        <p className="intro_text">원하시는 방식으로 로그인/회원가입을 진행해주세요.</p>
         <BaseButton
           type="button"
           style="solid__primary"
+          className="btn_intro_action"
+          onClick={onClickLoginEmail}
+        >
+          이메일로 로그인하기
+        </BaseButton>
+        <BaseButton
+          type="button"
+          style="outline__black"
+          className="btn_intro_action"
+          onClick={onClickLoginCertificate}
+        >
+          금융인증서로 로그인하기
+        </BaseButton>
+        <BaseButton
+          type="button"
+          style="line__black"
+          className="btn_intro_action"
           onClick={onClickGoJoin}
         >
-          회원가입 / 로그인 하러가기
+          아직 계정이 없으신가요? 회원가입
         </BaseButton>
       </Inner>
     </Screen>
