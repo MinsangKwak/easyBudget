58605159a457e7e2f01fb653db76488b50135f86
minsangKwak <kmsdevwork@gmail.com>
2025-12-18 01:56:07 +0000

    [add] setting

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    tabWidth: 4,            // 띄어쓰기 4칸
+    useTabs: false,
+    singleQuote: false,     // 쌍따옴표만 사용
+    semi: true,
+    trailingComma: "all",
+    printWidth: 100,
+    arrowParens: "always",
+};
--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,42 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from "eslint-plugin-storybook";
 
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+    globalIgnores(["dist"]),
+
+    {
+        files: ["**/*.{js,jsx}"],
+        extends: [
+            js.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+
+            // ✅ ESLint ↔ Prettier 충돌 제거
+            prettierConfig,
+        ],
+        plugins: {
+            prettier: prettierPlugin,
+        },
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                ecmaVersion: "latest",
+                ecmaFeatures: { jsx: true },
+                sourceType: "module",
+            },
+        },
+        rules: {
+            "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+            "prettier/prettier": "error",
+        },
     },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
-  },
-])
+]);
