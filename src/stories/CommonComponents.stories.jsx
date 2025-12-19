869e54bf7f097210cfc9da6f2bd8afcdcce1b2b8
minsangKwak <kmsdevwork@gmail.com>
2025-12-19 10:16:47 +0900

    Clean CSS comments and update Storybook

--- src/stories/CommonComponents.stories.jsx
+++ src/stories/CommonComponents.stories.jsx
@@ -1,6 +1,7 @@
+import IconLock from "../components/Common/IconLock";
+import LottieIcon from "../components/Common/LottieIcon";
 import ScreenLoading from "../components/Screen/Common/Loading";
-// import LottieIcon from "../components/Common/LottieIcon";
-// import { LOTTIE_SOURCES } from "../constants/lottieSources";
+import { LOTTIE_SOURCES } from "../constants/lottieSources";
 
 export default {
   title: "Components/Common",
@@ -16,13 +17,22 @@ export const LoadingIndicatorWithMessage = {
   render: () => <ScreenLoading message="로그인 중입니다." />,
 };
 
-// export const LottieIconPulse = {
-//   name: "LottieIcon",
-//   render: () => (
-//     <LottieIcon
-//       src={LOTTIE_SOURCES.introPulse}
-//       ariaLabel="애니메이션 예시"
-//       size={120}
-//     />
-//   ),
-// };
+export const SecurityLockIcon = {
+  name: "IconLock",
+  render: () => (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <IconLock />
+    </div>
+  ),
+};
+
+export const LottieIconPulse = {
+  name: "LottieIcon",
+  render: () => (
+    <LottieIcon
+      src={LOTTIE_SOURCES.introPulse}
+      ariaLabel="애니메이션 예시"
+      size={120}
+    />
+  ),
+};
