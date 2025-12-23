35e1718d3c58e0062f30717da0f639c83232624d
minsangKwak <56141969+MinsangKwak@users.noreply.github.com>
2025-12-23 08:00:24 +0000

    [merge] merge branch

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,3 @@
-
-
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import react from "@vitejs/plugin-react-swc";
@@ -9,36 +7,33 @@ const dirname =
 
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-vitest",
-    "@storybook/addon-a11y",
-    "@storybook/addon-docs"
-  ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  },
-  async viteFinal(baseConfig) {
-    const filteredPlugins = (baseConfig.plugins || []).filter(
-        (plugin) => plugin?.name !== "vite:react" && plugin?.name !== "vite:react-swc",
-    );
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    addons: [
+        "@chromatic-com/storybook",
+        "@storybook/addon-vitest",
+        "@storybook/addon-a11y",
+        "@storybook/addon-docs",
+    ],
+    framework: {
+        name: "@storybook/react-vite",
+        options: {},
+    },
+    async viteFinal(baseConfig) {
+        const filteredPlugins = (baseConfig.plugins || []).filter(
+            (plugin) => plugin?.name !== "vite:react" && plugin?.name !== "vite:react-swc",
+        );
 
-    return {
-        ...baseConfig,
-        plugins: [react(), ...filteredPlugins],
-        resolve: {
-            ...baseConfig.resolve,
-            alias: {
-                ...baseConfig.resolve?.alias,
-                "@": path.resolve(dirname, "../src"),
+        return {
+            ...baseConfig,
+            plugins: [react(), ...filteredPlugins],
+            resolve: {
+                ...baseConfig.resolve,
+                alias: {
+                    ...baseConfig.resolve?.alias,
+                    "@": path.resolve(dirname, "../src"),
+                },
             },
-        },
-    };
-  }
+        };
+    },
 };
 export default config;
