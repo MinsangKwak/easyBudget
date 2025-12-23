05a452dc7a8a2e1a1d4fada26ee2725f96ae5949
minsangKwak <56141969+MinsangKwak@users.noreply.github.com>
2025-12-23 16:52:24 +0900

    Fix Storybook stories and loading

--- src/stories/LayoutComponents.stories.jsx
+++ src/stories/LayoutComponents.stories.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Screen from "../components/Layout/Screen";
 import ScreenInnerGrid from "../components/Layout/ScreenInnerGrid";
 import AppHeader from "../components/Layout/AppHeader";
@@ -7,6 +8,31 @@ import BaseButton from "../components/Form/BaseButton";
 
 export default {
   title: "Components/Layout",
+};
+
+const HeaderExample = ({ isAuthenticated = false, showBackButton = false }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuItems = [
+    { key: "payment-methods", label: "지출 수단", onClick: () => {} },
+    { key: "category-spend", label: "카테고리 지출", onClick: () => {} },
+  ];
+
+  return (
+    <AppHeader
+      isAuthenticated={isAuthenticated}
+      showBackButton={showBackButton}
+      onBackClick={() => {}}
+      onLogoClick={() => {}}
+      onToggleMenu={() => setIsMenuOpen((previous) => !previous)}
+      onCloseMenu={() => setIsMenuOpen(false)}
+      isMenuOpen={isMenuOpen}
+      menuItems={menuItems}
+      onClickLogin={() => {}}
+      onClickSignUp={() => {}}
+      onClickLogout={() => {}}
+      onClickMyPage={() => {}}
+    />
+  );
 };
 
 export const BasicScreen = {
@@ -25,26 +51,20 @@ export const ScreenWithInnerGrid = {
   name: "ScreenInnerGrid",
   render: () => (
     <Screen>
-        <ScreenInnerGrid
-          top={<p>상단 콘텐츠</p>}
-          bottom={<BaseButton style="solid__primary">하단 버튼</BaseButton>}
-        />
-      </Screen>
+      <ScreenInnerGrid
+        top={<p>상단 콘텐츠</p>}
+        bottom={<BaseButton style="solid__primary">하단 버튼</BaseButton>}
+      />
+    </Screen>
   ),
 };
 
 export const AppHeaderWithBackButton = {
   name: "AppHeader",
-  render: () => <AppHeader showBackButton onBackClick={() => {}} />,
+  render: () => <HeaderExample showBackButton />,
 };
 
 export const AppHeaderWithAuthAction = {
   name: "AppHeader (Main)",
-  render: () => (
-    <AppHeader
-      isAuthenticated={false}
-      onLogoClick={() => {}}
-      onLoginClick={() => {}}
-    />
-  ),
+  render: () => <HeaderExample isAuthenticated />,
 };
--- src/stories/ScreenComponents.stories.jsx
+++ src/stories/ScreenComponents.stories.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import ScreenIntro from "../components/Screen/Intro";
 import ScreenMain from "../components/Screen/Main";
+import ScreenSpend from "../components/Screen/Spend";
+import ScreenCategory from "../components/Screen/Category";
+import ScreenProfile from "../components/Screen/Member/Profile";
 import ScreenLogin from "../components/Screen/Member/Login";
 import ScreenJoin from "../components/Screen/Member/Join";
 import ScreenJoinEmail from "../components/Screen/Member/JoinEmail";
@@ -9,27 +12,129 @@ import ScreenCertSelect from "../components/Screen/Member/Cert/ScreenCertSelect"
 import ScreenUser from "../components/Screen/Member/Cert/ScreenUser";
 import ScreenWait from "../components/Screen/Member/Cert/ScreenWait";
 import ScreenWelcome from "../components/Screen/Common/Welcome";
+import { useMainState } from "../components/Main/hooks/useMainState";
 import { bankList } from "../constants/bankList";
 
 export default {
   title: "Components/Screens",
 };
 
+const useDemoMainState = () => {
+  const mainState = useMainState({
+    isLinkedAccount: true,
+    ensureLinkedAccount: () => true,
+  });
+
+  const sectionIds = useMemo(
+    () => ({
+      paymentMethods: "story-payment-methods",
+      categorySpend: "story-category-spend",
+    }),
+    [],
+  );
+
+  return { mainState, sectionIds };
+};
+
 export const Intro = {
   name: "Intro",
-  render: () => <ScreenIntro onClickGoJoin={() => {}} />,
+  render: () => (
+    <ScreenIntro
+      onClickGoJoin={() => {}}
+      onClickLoginEmail={() => {}}
+      onClickLoginCertificate={() => {}}
+    />
+  ),
 };
 
 export const Login = {
   name: "Login",
   render: () => (
-    <ScreenLogin onLoginSuccess={() => {}} onClickSignUp={() => {}} />
+    <ScreenLogin
+      onLoginSuccess={() => {}}
+      onClickSignUp={() => {}}
+      onClickBack={() => {}}
+      onClickSwitchToCertificate={() => {}}
+    />
+  ),
+};
+
+export const LoginWithCertificate = {
+  name: "Login (Certificate)",
+  render: () => (
+    <ScreenLogin
+      mode="certificate"
+      onLoginSuccess={() => {}}
+      onClickSignUp={() => {}}
+      onClickBack={() => {}}
+      onClickSwitchToEmail={() => {}}
+    />
   ),
 };
 
 export const Main = {
   name: "Main",
-  render: () => <ScreenMain />, 
+  render: () => {
+    const Story = () => {
+      const { mainState } = useDemoMainState();
+
+      return (
+        <ScreenMain
+          onRequestSignUp={() => {}}
+          isLinkedAccount
+          isSignUpModalOpen={false}
+          onCloseSignUpModal={() => {}}
+          mainState={mainState}
+        />
+      );
+    };
+
+    return <Story />;
+  },
+};
+
+export const Spend = {
+  name: "Spend",
+  render: () => {
+    const Story = () => {
+      const { mainState, sectionIds } = useDemoMainState();
+
+      return (
+        <ScreenSpend
+          onRequestSignUp={() => {}}
+          isLinkedAccount
+          isSignUpModalOpen={false}
+          onCloseSignUpModal={() => {}}
+          mainState={mainState}
+          sectionIds={sectionIds}
+        />
+      );
+    };
+
+    return <Story />;
+  },
+};
+
+export const Category = {
+  name: "Category",
+  render: () => {
+    const Story = () => {
+      const { mainState, sectionIds } = useDemoMainState();
+
+      return (
+        <ScreenCategory
+          onRequestSignUp={() => {}}
+          isLinkedAccount
+          isSignUpModalOpen={false}
+          onCloseSignUpModal={() => {}}
+          mainState={mainState}
+          sectionIds={sectionIds}
+        />
+      );
+    };
+
+    return <Story />;
+  },
 };
 
 export const Join = {
@@ -42,6 +147,11 @@ export const Join = {
 export const JoinEmail = {
   name: "JoinEmail",
   render: () => <ScreenJoinEmail onSignUpComplete={() => {}} />,
+};
+
+export const Profile = {
+  name: "Profile",
+  render: () => <ScreenProfile onGoHome={() => {}} onDeleteAccount={() => {}} />,
 };
 
 export const CertificateFlow = {
